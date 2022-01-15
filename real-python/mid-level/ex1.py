4eb1a8d8036c027e29b6c796d50118983291784d
gerard300 <53865161+gerard300@users.noreply.github.com>
2022-01-15 19:00:20 +0100

    Create ex1.py
    Created

---
+++ real-python/mid-level/ex1.py
@@ -0,0 +1,21 @@
+def keypad_string(keys):
+    '''
+    Given a string consisting of 0-9,
+    find the string that is created using
+    a standard phone keypad
+    | 1        | 2 (abc) | 3 (def)  |
+    | 4 (ghi)  | 5 (jkl) | 6 (mno)  |
+    | 7 (pqrs) | 8 (tuv) | 9 (wxyz) |
+    |     *    | 0 ( )   |     #    |
+    You can ignore 1, and 0 corresponds to space
+    >>> keypad_string("12345")
+    'adgj'
+    >>> keypad_string("4433555555666")
+    'hello'
+    >>> keypad_string("2022")
+    'a b'
+    >>> keypad_string("")
+    ''
+    >>> keypad_string("111")
+    ''
+    '''
